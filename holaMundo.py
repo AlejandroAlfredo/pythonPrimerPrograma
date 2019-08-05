cece725c0d7aeef33eb8fe2bb8e8e945f298454d
Alejandro <alejandro23aq01@hotmail.com>
2019-08-05 12:49:56 -0500

    mi primeros archivos python

---
+++ holaMundo.py
@@ -0,0 +1,7 @@
+
+valorUno = int(input("Dame un valor: "))
+valorDos = int(input("Dame el segundo valor: "))
+
+resultado = valorUno + valorDos
+
+print("La suma es: " + str(resultado))
---
+++ resta.py
@@ -0,0 +1,7 @@
+valorUno = int(input("Dame el primer valor: "))
+
+valorDos = int(input("Dame el segundo valor: "))
+
+resultado = valorUno - valorDos
+
+print("El resultado de la resta es: " + str(resultado))
