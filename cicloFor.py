e9246cd9b3debb42bbd5293eaf7c93b34d279dc0
Alejandro <alejandro23aq01@hotmail.com>
2019-08-06 13:28:42 -0500

    mi primer ciclo

---
+++ cicloFor.py
@@ -0,0 +1,12 @@
+#ciclos
+for value in range (8):
+        print(value)
+#nuevo for
+valueOne = int(input("Cuantas veces quieres imprimir: "))
+for value in range (valueOne):
+    print(value + 1)
+#ciclo entre 5 - 8
+valueOne = int(input("Escribe el inicio que quieres imprimir: "))
+valueTwo = int(input("Escribe el limite de la impresion: "))
+for value in range (valueOne, valueTwo):
+    print(value)
