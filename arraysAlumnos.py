d5ba2286f2ebbfaa9a55360c8cca8dbcc81bd4f2
Alejandro <alejandro23aq01@hotmail.com>
2019-08-08 12:30:07 -0500

    ejemplo de alumnos

---
+++ arraysAlumnos.py
@@ -0,0 +1,13 @@
+#imprimir alumonos en arryas
+alumnos = []
+alumnos.append('juan')
+alumnos.append('pedro')
+alumnos.append('miguel')
+for alumno in alumnos:
+    print(alumno)
+alumnos.append('alejandro')
+print(alumnos)
+print(alumnos[2])
+print((len(alumnos)))
+print(alumnos[0])
+print((alumnos[3]))
