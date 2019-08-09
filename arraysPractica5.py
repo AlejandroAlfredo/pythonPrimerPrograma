562e6a55237ddd923466d45e34c36402fcdd6107
Alejandro <alejandro23aq01@hotmail.com>
2019-08-09 16:15:17 -0500

    practica con arrays 5

---
+++ arraysPractica5.py
@@ -0,0 +1,11 @@
+# uso de remove y pop
+# arrays quitar
+elementos = []
+elementos.append("frutas")
+elementos.append("autos")
+elementos.append("verduras")
+
+print(elementos)
+#elementos.remove("autos")
+elementos.pop(2)
+print(elementos)
