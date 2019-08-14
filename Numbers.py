83ceb9b9c4c1ad5fa074bfad1ee5210a9aa7e28e
Alejandro <alejandro23aq01@hotmail.com>
2019-08-14 15:10:27 -0500

    numbers

---
+++ Numbers.py
@@ -0,0 +1,12 @@
+# Numbers
+print(type(12))
+print(type(18.3))
+print(int(18.3))
+print(int("19"))
+# print(int("xx"))
+print(float(19))
+print(float("19.20"))
+print(float("19"))
+print(str(1))
+print(str(1.5))
+# int, float, Boolean = datos primitivos
