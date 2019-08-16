7cc3dafe21b2bd2ecfd0eab72dafeabf69372f3b
Alejandro <alejandro23aq01@hotmail.com>
2019-08-16 13:16:49 -0500

    mi primer datatime

---
+++ DateTime.py
@@ -0,0 +1,12 @@
+import datetime
+x = datetime.datetime.now()
+#print(x) fecha
+#print(x.year) año
+#print(x.strftime('%A')) lun,mar,mie...
+#print(x.strftime('%b'))
+#print(x.strftime('%a'))
+#print(x.strftime('%W'))
+#print(x.month) mes
+#print(x.day) dias
+x = datetime.datetime(2019,5,17)
+print(x)
