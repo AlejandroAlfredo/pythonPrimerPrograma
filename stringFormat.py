b00765aa5e9f18c3633b377e3d45f1061a783959
Alejandro <alejandro23aq01@hotmail.com>
2019-08-16 15:53:02 -0500

    mi primer string Format

---
+++ stringFormat.py
@@ -0,0 +1,7 @@
+#String format
+myName = "Alejandro"
+#myFormat = "Hola " + myName + " estoy en clases"
+myClass = "Programacion"
+myMoney = 150.9090
+myFormat = "hola {} estoy en clases {} y traigo {:.2f}"
+print(myFormat.format(myName,myClass,myMoney))
